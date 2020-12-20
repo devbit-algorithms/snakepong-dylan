11a45ec0b5d1b87afa6f1b7797c677e216656236
Dylan Missuwe <dylan.missu@gmail.com>
2020-12-20 18:51:56 +0100

    added basic user input

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ pygame.init()
 display = (400, 400)
 game = pygame.display.set_mode(display)
 
-while True:
-    pygame.draw.rect(game,(255,255,255),[100,100,10,10])
+blockPosX = 100
+blockPosY = 100
+
+blockSize = 20
+
+gameEnded = False
+while not gameEnded:
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    blockPosX += -blockSize
+                elif event.key == pygame.K_RIGHT:
+                    blockPosX += blockSize
+                elif event.key == pygame.K_UP:
+                    blockPosY += -blockSize
+                elif event.key == pygame.K_DOWN:
+                    blockPosY += blockSize
+    pygame.draw.rect(game,(255,255,255),[blockPosX,blockPosY,blockSize,blockSize])
     pygame.display.update()
 
 pygame.quit()
