51185e180e17f2cc714a355af7eae69add63c5c5
Dylan Missuwe <dylan.missu@gmail.com>
2020-12-24 00:14:19 +0100

    now utilizing the single linked list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from sllist import LinkedList
+from sllist import SingleLinkedList
 import time
 
 pygame.init()
@@ -7,22 +7,28 @@ pygame.init()
 display = (400, 400)
 game = pygame.display.set_mode(display)
 
-# initial position
-blockPosX = 100
-blockPosY = 100
-
 # block size
-blockSize = 20
+blockSize = 10
 
-# how fast and in what direction does the snake move
 velocityX = 0
 velocityY = 0
 
-#snake = LinkedList()
+snake = SingleLinkedList()
+snake.prepend(((0, 0), (100, 100), (255,255,255)))
+
+snake.printToConsole()
 
 gameEnded = False
+
+def showSnake(snake):
+    if snake.head() is not None:
+        pygame.draw.rect(game,snake.head()[2],[snake.head()[1][0], snake.head()[1][1], blockSize, blockSize])
+        showSnake(snake.tail())
+
 while not gameEnded:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameEnded = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 velocityX = -blockSize
@@ -37,14 +43,16 @@ while not gameEnded:
                 velocityY = blockSize
                 velocityX = 0
 
-    blockPosX += velocityX
-    blockPosY += velocityY
-    print(velocityX)
-    print(velocityY)
+    update = (
+        (velocityX, velocityY),
+        (snake.head()[1][0] + snake.head()[0][0], snake.head()[1][1] + snake.head()[0][1]),
+        (255, 255, 255))
 
-    pygame.draw.rect(game,(255,255,255),[blockPosX, blockPosY, blockSize, blockSize])
+    snake.update(update)
+
+    showSnake(snake)
     pygame.display.update()
     pygame.draw.rect(game,(0,0,0),[0,0,display[0],display[1]])
-    time.sleep(0.5)
+    time.sleep(0.3)
 
 pygame.quit()
--- sllist.py
+++ sllist.py
@@ -1,60 +1,126 @@
-class Node(object):
+# defines a node in a singly linked list
+class _Node:
+    # constructs a node with an element and a next node 
+    def __init__(self, element, nextNode):
+        self.__element = element
+        self.__next = nextNode
 
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
+    # returns the element at the node
+    def get(self):
+        return self.__element
 
-    def get_data(self):
-        return self.data
+    # returns the element at the node
+    def update(self, newElement):
+        self.__element = newElement
+    
+    # returns the next node
+    def next(self):
+        return self.__next
 
-    def get_next(self):
-        return self.next_node
+# defines a singly linked list
+class SingleLinkedList:
+    # constructs an empty singly linked list or if provided a _Node a singly linked list starting with the provided _Node
+    def __init__(self, first = None):
+        self.__head = first
 
-    def set_next(self, new_next):
-        self.next_node = new_next
-
-class LinkedList(object):
-    def __init__(self, head=None):
-        self.head = head
-
-    def insert(self, data):
-        new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
-
-    def size(self):
-        current = self.head
-        count = 0
-        while current:
-            count += 1
-            current = current.get_next()
-        return count
-
-    def search(self, data):
-        current = self.head
-        found = False
-        while current and found is False:
-            if current.get_data() == data:
-                found = True
-            else:
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        return current
-
-    def delete(self, data):
-        current = self.head
-        previous = None
-        found = False
-        while current and found is False:
-            if current.get_data() == data:
-                found = True
-            else:
-                previous = current
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        if previous is None:
-            self.head = current.get_next()
+    # returns the first element in the singly linked list or None if empty
+    def head(self):
+        if self.isEmpty():
+            return None
         else:
-            previous.set_next(current.get_next())
+            return self.__head.get()
+
+    # updates the first element in the singly linked list or not if empty
+    def update(self, newElement):
+        if self.isEmpty():
+            return None
+        else:
+            return self.__head.update(newElement)
+    
+    # returns a singly linked list containing all but the first element
+    def tail(self):
+        if self.isEmpty():
+            return SingleLinkedList()
+        else:
+            return SingleLinkedList(self.__head.next())
+
+    # adds an element at the front of the singly linked list
+    def prepend(self, element):
+        self.__head = _Node(element, self.__head)
+        return self
+
+    # returns True if the singly linked list does not contain any elements
+    def isEmpty(self):
+        if self.__head is None:
+            return True
+        else:
+            return False
+    
+    # returns the number of elements in the list
+    def size(self):
+        def count(n, node):
+            if node is None:
+                return n
+            else:
+                return count(n+1, node.next())
+
+        return count(0, self.__head)
+
+    # places new list in front of current list
+    def join(self, newList):
+        def joinLoop(listA, listB):
+            if not listB.isEmpty():
+                joinLoop(listA.prepend(listB.head()), listB.tail())
+
+        joinLoop(self, newList.reverse())
+
+    # prints all elements to console
+    def printToConsole(self):
+        def printLoop(list):
+            if not list.isEmpty():
+                print(list.head())
+                printLoop(list.tail())
+
+        printLoop(self.reverse())
+
+    # checks if a certain element is in the SingleLinkedList
+    def contains(self, element):
+        def containsLoop(element, list):
+            if list.head() == element:
+                return True
+            elif list.isEmpty():
+                return False
+            else:
+                return containsLoop(element, list.tail())
+
+        return containsLoop(element, self)
+
+    def reverse(self):
+        newList = SingleLinkedList()
+        def reverseLoop(startList, list):
+            if not list.isEmpty():
+                reverseLoop(startList.prepend(list.head()), list.tail())
+        
+        reverseLoop(newList, self)
+        self = newList
+        return self
+
+    def copy(self):
+        newList = SingleLinkedList()
+        def copyLoop(startList, list):
+            if not list.isEmpty():
+                copyLoop(startList.prepend(list.head()), list.tail())
+        
+        copyLoop(newList, self)
+        return newList.reverse()
+
+
+def Average(list):
+    def averageLoop(val, n, list):
+        if list.isEmpty():
+            return val / n
+        else:
+            n += 1
+            val += list.head()
+            return averageLoop(val, n, list.tail())
+    return averageLoop(0, 0, list)
